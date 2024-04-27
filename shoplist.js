aeac6f451e7ac8c1a1f0530a5460bd13988ab79d
hencci <hencci20@gmail.com>
2024-04-27 18:09:47 +0100

    Created 3 new elements and stored them in seperate variables.

--- shoplist.js
+++ shoplist.js
@@ -4,5 +4,9 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
     const myItem = input.value;
-    input.value = "";
+    input.value = " ";
+
+    const listItem = document.createElement("li");
+    const listContent = document.createElement("span");
+    const listButton = document.createElement("button");
 });
