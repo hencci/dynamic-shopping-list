2d3c9a3077dcfaec0439bbc725bbec1400a60650
hencci <hencci20@gmail.com>
2024-04-27 18:02:18 +0100

    Created a function to respond to the click even of the button, and stored the current value of the input

--- shoplist.js
+++ shoplist.js
@@ -1,3 +1,8 @@
 const list = document.querySelector("ul");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    const myItem = input.value;
+    input.value = "";
+});
